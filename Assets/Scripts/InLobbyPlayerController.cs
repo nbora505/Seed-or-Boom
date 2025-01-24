50904bfb1dfb8afb2c450e2650c9e274a54941d8
GGEU <daehan121993@gmail.com>
2025-01-24 17:11:50 +0900

    InRobbyPlayerController

--- Assets/Scripts/InLobbyPlayerController.cs
+++ Assets/Scripts/InLobbyPlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class InLobbyPlayerController : MonoBehaviour
 {
+    public GameObject multiCanvas;
+    public GameObject singleCanvas;
+    public GameObject playerInfoCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        multiCanvas.SetActive(false);
+        singleCanvas.SetActive(false);
+        playerInfoCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "MultiPlayColider") multiCanvas.SetActive(true);
+        if (other.gameObject.name == "PlayerInfoColider") playerInfoCanvas.SetActive(true);
+        if (other.gameObject.name == "SinglePlayColider") singleCanvas.SetActive(true);
     }
 }
