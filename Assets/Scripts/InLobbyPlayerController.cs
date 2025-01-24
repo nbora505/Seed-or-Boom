3cfb3612a6ddfdceb07a6f0148d0f5cd52af26c5
GGEU <daehan121993@gmail.com>
2025-01-24 18:11:32 +0900

    Add TweenAnimation

--- Assets/Scripts/InLobbyPlayerController.cs
+++ Assets/Scripts/InLobbyPlayerController.cs
@@ -33,14 +33,35 @@ public class InLobbyPlayerController : MonoBehaviour
             multiCanvas.SetActive(true);
             multiCanvas.transform.DOScale(temp, 1f);
         }
-        if (other.gameObject.name == "PlayerInfoColider") playerInfoCanvas.SetActive(true);
-        if (other.gameObject.name == "SinglePlayColider") singleCanvas.SetActive(true);
+        if (other.gameObject.name == "PlayerInfoColider")
+        {
+            Vector3 temp = playerInfoCanvas.transform.localScale;
+            playerInfoCanvas.transform.localScale = Vector3.zero;
+            playerInfoCanvas.SetActive(true);
+            playerInfoCanvas.transform.DOScale(temp, 1f);
+        }
+        if (other.gameObject.name == "SinglePlayColider")
+        {
+            Vector3 temp = singleCanvas.transform.localScale;
+            singleCanvas.transform.localScale = Vector3.zero;
+            singleCanvas.SetActive(true);
+            singleCanvas.transform.DOScale(temp, 1f);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "MultiPlayColider") multiCanvas.SetActive(false);
-        if (other.gameObject.name == "PlayerInfoColider") playerInfoCanvas.SetActive(false);
-        if (other.gameObject.name == "SinglePlayColider") singleCanvas.SetActive(false);
+        if (other.gameObject.name == "MultiPlayColider")
+        {
+            multiCanvas.transform.DOScale(Vector3.zero, 1f).OnComplete(() => multiCanvas.SetActive(false));
+        }
+        if (other.gameObject.name == "PlayerInfoColider")
+        {
+            playerInfoCanvas.transform.DOScale(Vector3.zero, 1f).OnComplete(() => multiCanvas.SetActive(false));
+        }
+        if (other.gameObject.name == "SinglePlayColider")
+        {
+            singleCanvas.transform.DOScale(Vector3.zero, 1f).OnComplete(() => multiCanvas.SetActive(false));
+        }
     }
 }
