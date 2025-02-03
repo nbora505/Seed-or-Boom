64b8cd811728df987424e26e5e0b0adb2da0270d
GGueMMue <daehan121993@gmail.com>
2025-02-03 20:17:48 +0900

    롤백

--- Assets/Scripts/Multiplay_Scripts_Folder/OVRPlayerControllerPhotonSync.cs
+++ Assets/Scripts/Multiplay_Scripts_Folder/OVRPlayerControllerPhotonSync.cs
@@ -2,106 +2,52 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.SpatialTracking;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class OVRPlayerControllerPhotonSync : MonoBehaviourPunCallbacks
 {
     public GameObject cardObj;
     public GameObject ovrCamera;
-    GameObject tempOVR;
     HeadBodyRig hbr;
 
-    void Awake()
-    {
-        hbr = GetComponent<HeadBodyRig>();
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
-        if (photonView.IsMine)
+        if(photonView.IsMine)
         {
             GameObject ovr = Instantiate(ovrCamera);
             ovr.transform.SetParent(this.gameObject.transform, false);
-            tempOVR = ovr;
+            
+            hbr = GetComponent<HeadBodyRig>();
 
-            SetupVRTargets();
-
-            photonView.RPC("SyncVRSetup", RpcTarget.Others);
+            hbr.head.VRTarget = ovr.GetComponent<GetVRTrackingPosition>().ReturnCenterEyeAnchor();
+            hbr.rightHand.VRTarget = ovr.GetComponent<GetVRTrackingPosition>().ReturnRightHandAnchor();
+            hbr.leftHand.VRTarget = ovr.GetComponent<GetVRTrackingPosition>().ReturnLeftHandAnchor();
         }
     }
-
-    void FixedUpdate()
-    {
-        if (photonView.IsMine)
-        {
-            photonView.RPC("UpdateRemotePlayerTransform", RpcTarget.Others,
-                hbr.head.VRTarget.position, hbr.head.VRTarget.rotation,
-                hbr.rightHand.VRTarget.position, hbr.rightHand.VRTarget.rotation,
-                hbr.leftHand.VRTarget.position, hbr.leftHand.VRTarget.rotation);
-        }
-    }
-
-    void SetupVRTargets()
-    {
-        var trackingComponent = tempOVR.GetComponent<GetVRTrackingPosition>();
-        if (trackingComponent != null)
-        {
-            hbr.head.VRTarget = trackingComponent.ReturnCenterEyeAnchor();
-            hbr.rightHand.VRTarget = trackingComponent.ReturnRightHandAnchor();
-            hbr.leftHand.VRTarget = trackingComponent.ReturnLeftHandAnchor();
-        }
-    }
-
-    [PunRPC]
-    void UpdateRemotePlayerTransform(Vector3 headPos, Quaternion headRot,
-                                     Vector3 rightHandPos, Quaternion rightHandRot,
-                                     Vector3 leftHandPos, Quaternion leftHandRot)
-    {
-        if (hbr.head.VRTarget != null)
-        {
-            hbr.head.VRTarget.position = headPos;
-            hbr.head.VRTarget.rotation = headRot;
-        }
-        if (hbr.rightHand.VRTarget != null)
-        {
-            hbr.rightHand.VRTarget.position = rightHandPos;
-            hbr.rightHand.VRTarget.rotation = rightHandRot;
-        }
-        if (hbr.leftHand.VRTarget != null)
-        {
-            hbr.leftHand.VRTarget.position = leftHandPos;
-            hbr.leftHand.VRTarget.rotation = leftHandRot;
-        }
-    }
-
-    [PunRPC]
-    void SyncVRSetup()
-    {
-        var trackingComponent = GetComponentInChildren<GetVRTrackingPosition>();
-        if (trackingComponent != null)
-        {
-            hbr.head.VRTarget = trackingComponent.ReturnCenterEyeAnchor();
-            hbr.rightHand.VRTarget = trackingComponent.ReturnRightHandAnchor();
-            hbr.leftHand.VRTarget = trackingComponent.ReturnLeftHandAnchor();
-        }
-    }
-
+    // Update is called once per frame
     void Update()
     {
+
         if (!photonView.IsMine)
         {
             return;
         }
 
-        if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        if(OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
         {
             cardObj.SetActive(true);
+            // card apear
         }
 
-        if (OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger))
+        if(OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger))
         {
             cardObj.SetActive(false);
+            // card disapear
         }
     }
 }
