06f65f03e558fd92753562f734aa30dd93998336
GGueMMue <daehan121993@gmail.com>
2025-02-04 22:09:18 +0900

    Update MultiplayGameManager.cs

--- Assets/Scripts/Multiplay_Scripts_Folder/MultiplayGameManager.cs
+++ Assets/Scripts/Multiplay_Scripts_Folder/MultiplayGameManager.cs
@@ -245,8 +245,23 @@ public class MultiplayGameManager : MonoBehaviourPunCallbacks, IPunObservable
         // 다음 플레이어의 승수 입력 순서를 진행 (현재 순서는 playerIndex 기준이 아닌 leader 기준의 순번)
         if (PhotonNetwork.IsMasterClient)
         {
+            int currentOrder;
+
+            if (playerIndex >= leaderIndex)
+            {
+                currentOrder = playerIndex - leaderIndex;
+            }
+            else
+            {
+                currentOrder = playerList.Count - leaderIndex + playerIndex;
+            }
+
+            int nextOrder = currentOrder + 1;
+
+            Debug.LogError($"Current playerIndex: {playerIndex}, Leader: {leaderIndex}, CurrentOrder: {currentOrder}, NextOrder: {nextOrder}");
+
             // 다음 순서는 현재 playerIndex + 1를 의미 (leader 기준 순서 계산)
-            photonView.RPC("RPC_ProcessDecideWinCount", RpcTarget.AllBuffered, playerIndex - leaderIndex + 1);
+            photonView.RPC("RPC_ProcessDecideWinCount", RpcTarget.AllBuffered, nextOrder);
         }
     }
 
@@ -254,7 +269,7 @@ public class MultiplayGameManager : MonoBehaviourPunCallbacks, IPunObservable
     [PunRPC]
     void RPC_ProcessDecideWinCount(int nextPlayerOrder)
     {
-        if (nextPlayerOrder > playerList.Count)
+        if (nextPlayerOrder >= playerList.Count)
         {
             winCountSelectionComplete = true;
             return;
