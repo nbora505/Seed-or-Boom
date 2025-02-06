203b32cd7dad8920f8948355d5b95a538d429ace
JOHJIN <8pp8pp@naver.com>
2025-02-06 12:02:32 +0900

    로그인 하이어라키 연결

--- Assets/Scripts/Keyboard/KeyBoardControllerLogIN.cs
+++ Assets/Scripts/Keyboard/KeyBoardControllerLogIN.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KeyBoardControllerLogIN: MonoBehaviour
+public class KeyBoardControllerLogIN : MonoBehaviour
 {
     public GameObject keyBoardPanel;
     public GameObject nickPanel;
